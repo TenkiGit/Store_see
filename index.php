d151a67b2599850bbc721543b6278655eab85551
Tenki <tenki1231@gmail.com>
2018-11-28 21:13:27 +0900

    fix_s

--- index.php
+++ index.php
@@ -15,16 +15,7 @@ function main(){
 	//テンプレートを指定
 	$template = './template/index.html';
 	$params["user_data"] = [];
-	$filepath="./data.csv";
-
-	 $fp = fopen($filepath, "r");
-while ($data = fgetcsv($fp, 10000)) {
-  print "<tr>";
-  foreach ($data as $d) {
-    print "<td>$d</td>";
-  }
-  print "</tr>\n";
-}
+	
 
 	$contents = common::html_output($template,$params);
 
