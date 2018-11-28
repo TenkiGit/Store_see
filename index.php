29fb7e2f2b7503112faae328e38282b5b235937d
Tenki <tenki1231@gmail.com>
2018-11-28 21:05:14 +0900

    add_json

--- index.php
+++ index.php
@@ -15,9 +15,15 @@ function main(){
 	//テンプレートを指定
 	$template = './template/index.html';
 	$params["user_data"] = [];
-	
+	$filepath="./data.csv";
 
-	
+	 $arr = json_decode($json,true);
+
+    echo "<pre>";
+    foreach($arr as $v) {
+      var_dump($v["s_name"]);
+    }
+    echo "</pre>";
 
 	$contents = common::html_output($template,$params);
 
