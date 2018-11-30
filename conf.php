e9d8fa142846ca7dcdb6ced4da0cdab2aa102dcb
Tenki <tenki1231@gmail.com>
2018-11-30 16:08:45 +0900

    ch

--- conf.php
+++ conf.php
@@ -32,22 +32,22 @@ function main(){
     //if-post==csv->OnFlags
     foreach($csv as $key=>$data ){
    
-        // var_dump($data["1.店舗名"]);
+        var_dump($data);
         if($data["1.店舗名"]==$_POST["name"]){
             $count+=1;
         }
     }
     if($count==1){
         $inp_data= ["1.店舗名"=>$_POST["name"],"2.出勤者"=>$_POST["come_m"],"3.出前者"=>$_POST["out_m"],"4.店舗在任"=>$_POST["store_in"],"5.必要人数"=>$_POST["need_m"]];
-        foreach($csv as $key=>$data ){
-            if($data["1.店舗名"]==$_POST["name"]){
-                $data["1.店舗名"]=$_POST["name"];
-                $data["2.出勤者"]=$_POST["come_m"];
-                $data["3.出前者"]=$_POST["out_m"];
-                $data["4.店舗在任"]=$_POST["store_in"];
-                $data["5.必要人数"]=$_POST["need_m"];
-            }
-        }
+        // foreach($csv as $key=>$data ){
+        //     if($data["1.店舗名"]==$_POST["name"]){
+        //         $data["1.店舗名"]=$_POST["name"];
+        //         $data["2.出勤者"]=$_POST["come_m"];
+        //         $data["3.出前者"]=$_POST["out_m"];
+        //         $data["4.店舗在任"]=$_POST["store_in"];
+        //         $data["5.必要人数"]=$_POST["need_m"];
+        //     }
+        // }
         // //書き込み
         // $fp = fopen('data2.csv', 'w');
         // foreach($data as $data2){
@@ -55,7 +55,7 @@ function main(){
 	    //     fwrite($fp, $line . "\n");
         // }
         // fclose($fp);
-        // $csv=array_merge($csv, $inp_data);
+        $csv=array_merge($csv, $inp_data);
         var_dump($count);
         var_dump($csv);
         // var_dump($data);
