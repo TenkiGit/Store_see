b80d66724f623da96e7dcabe2ab2c6441b699e1f
Tenki <tenki1231@gmail.com>
2018-12-02 17:26:11 +0900

    array_merge

--- conf.php
+++ conf.php
@@ -41,7 +41,7 @@ function main(){
         $inp_data= ["1.店舗名"=>$_POST["name"],"2.出勤者"=>$_POST["come_m"],"3.出前者"=>$_POST["out_m"],"4.店舗在任"=>$_POST["store_in"],"5.必要人数"=>$_POST["need_m"]];
         foreach($csv as $key=>$data ){
             if($data["1.店舗名"]==$_POST["name"]){
-                $data=array_replace($data, $inp_data);
+                $data=array_merge($data, $inp_data);
                 var_dump($data);
             }
         }
